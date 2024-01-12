245b334867be9b273e240164c4e69ddd97582935
JENkt4k <james.e.nelson@gmail.com>
2024-01-12 06:30:52 -0600

    checl

---
+++ scripts/app.js
@@ -0,0 +1,38 @@
+let selected = null;
+
+import { db, addTaskToDB, fetchTasksFromDB } from './db.js';
+
+import { loadListControl, loadTasks } from './taskList.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+  // Initialize the app
+  initApp();
+});
+
+function initApp() {
+  // Set up event listeners for UI elements
+  const addButton = document.getElementById('add-task-button');
+  
+  if(addButton) {
+    addButton.addEventListener('click', addTask);
+  } 
+  // Add other event listeners as needed
+  loadListControl();
+}
+
+function addTask() {
+  const newTaskInput = document.getElementById('new-task');
+  const task = newTaskInput.value;
+  newTaskInput.value = ''; // Clear the input field
+
+  if (task) {
+      // Add the task to IndexedDB
+      addTaskToDB(task).then(() => {
+          // Update the task list display
+          loadTasks();
+      }).catch(error => console.error(error));
+  }
+}
+
+
+
